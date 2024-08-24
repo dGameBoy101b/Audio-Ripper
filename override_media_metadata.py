75a17b07a8977bdfb0726b1aa9c6c47036f4dec5
dGameBoy101b <rhysmader@gmail.com>
2024-08-24 16:17:59 +0800

    Created override meta data

---
+++ override_media_metadata.py
@@ -0,0 +1,16 @@
+import ffmpeg
+
+#https://github.com/kkroening/ffmpeg-python/issues/112
+def override_media_metadata(**overrides)->dict[str, str]:
+	args=dict({'map_metadata':0})
+	index=0
+	for key in overrides:
+		args[f'metadata:g:{index}']=key+'='+overrides[key]
+		index+=1
+	return args
+
+if __name__=='__main__':
+	overrides = {'author':'me','year':'2024'}
+	print(overrides)
+	mapped = override_media_metadata(**overrides)
+	print(mapped)
