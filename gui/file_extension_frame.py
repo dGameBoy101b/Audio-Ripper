ebaa5e126802cbb2146bb7360a7a5d3f238d0384
dGameBoy101b <rhysmader@gmail.com>
2024-10-29 02:40:38 +0800

    Added get method to file extension frame

--- gui/file_extension_frame.py
+++ gui/file_extension_frame.py
@@ -5,10 +5,15 @@ class FileExtensionFrame(ttk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.columnconfigure(1, weight=1)
-		self._create_child_widgets()
-
-	def _create_child_widgets(self):
+		
 		self.label = ttk.Label(self, text='File Extension: ')
 		self.label.grid(column=0, row=0, sticky='W')
+
 		self.entry = ttk.Entry(self, width=5)
 		self.entry.grid(column=1, row=0, sticky='EW')
+
+	def get(self):
+		value = self.entry.get()
+		if value[0] != '.':
+			return '.'+value
+		return value
