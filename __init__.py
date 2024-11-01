7010f89be3cc2eb7904cfd2c4f4ebdcb165547d0
dGameBoy101b <rhysmader@gmail.com>
2024-11-01 12:16:39 +0800

    Created package

---
+++ __init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+	'rip_args',
+	'rip_report',
+	'scan_for_audio',
+	'change_file_extension',
+	'override_media_metadata',
+	'copy_media',
+	'threaded_rip',
+	'sequential_rip',
+	'multiprocessing_rip',
+	'audio_ripper'
+]
