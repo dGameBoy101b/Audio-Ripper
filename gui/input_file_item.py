49951c97ffd822666507521f66bc39da0fcc785e
dGameBoy101b <rhysmader@gmail.com>
2024-11-02 02:54:00 +0800

    added logging to input file item. changed input file item to contain a readonly entry instead of a label.

--- gui/input_file_item.py
+++ gui/input_file_item.py
@@ -1,25 +1,34 @@
 from tkinter import ttk
 from os import PathLike
+import tkinter
+from logging import getLogger
 
 class InputFileItem(ttk.Frame):
 
-	def __init__(self, path: PathLike, master=None, on_remove=None):
-		super().__init__(master)
+	def __init__(self, path: PathLike, master=None, on_remove=None, **kwargs):
+		logger = getLogger(__name__)
+		super().__init__(master, **kwargs)
+		logger.debug('created input file item')
 
 		self.path = path
 		self.on_remove = on_remove
+		logger.debug('setup input file item variables')
 
-		self.text = ttk.Label(self, text=str(path), width=20)
-		self.remove_button = ttk.Button(self, text='x', command=self.remove)
+		self.variable = tkinter.StringVar(master=self, value=str(path))
+		self.text = ttk.Entry(self, textvariable=self.variable, width=30, state='readonly')
+		self.remove_button = ttk.Button(self, text='x', command=self.remove, width=2)
+		logger.debug('created input file item children')
 
-		self.text.grid(column=0, row=0)
+		self.text.grid(column=0, row=0, sticky='EW')
 		self.remove_button.grid(column=1, row=0)
 		self.columnconfigure(0, weight=1)
+		logger.debug('layed out input file item')
 
 	def remove(self):
+		logger = getLogger(__name__)
 		self.on_remove(self)
 		self.destroy()
-		print(f'removed input path: {self.path}')
+		logger.debug(f'removed input path: {self.path}')
 
 	def get(self)->PathLike:
 		return self.path
