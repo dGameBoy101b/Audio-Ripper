30be37154d6148c82bb5bc0acad8cdf9ae0794ae
dGameBoy101b <rhysmader@gmail.com>
2024-11-02 02:05:48 +0800

    added logging to audio ripper gui

--- gui/audio_ripper_gui.py
+++ gui/audio_ripper_gui.py
@@ -3,34 +3,46 @@ from .settings_frame import SettingsFrame
 from .input_frame import InputFrame
 from .output_frame import OutputFrame
 import logging
+from os import path
 
 class AudioRipperGUI(ttk.Frame):
 
 	def __init__(self, master=None):
+		logger = logging.getLogger(__name__)
 		ttk.Frame.__init__(self, master)
+		logger.debug('created audio ripper gui')
+
+		self.settingsFrame = SettingsFrame(self)
+		self.inputFrame = InputFrame(self)
+		self.outputFrame = OutputFrame(self)
+		logger.debug('created audio ripper gui children')
+
 		self.grid(sticky='NSEW')
 		self.rowconfigure(0, weight=1)
 		self.columnconfigure([0,1,2], weight=1)
-		self._create_child_widgets()
-
-	def _create_child_widgets(self):
-		self.settingsFrame = SettingsFrame(self)
 		self.settingsFrame.grid(column=0, row=0, sticky='NSEW')
-		self.inputFrame = InputFrame(self)
 		self.inputFrame.grid(column=1, row=0, sticky='NSEW')
-		self.outputFrame = OutputFrame(self)
 		self.outputFrame.grid(column=2, row=0, sticky='NSEW')
+		logger.debug('layed out audio ripper gui')
 
 	def configure_window(self):
+		logger = logging.getLogger(__name__)
 		window = self.winfo_toplevel()
 		window.title('Audio Ripper')
 		window.columnconfigure(0, weight=1)
 		window.rowconfigure(0, weight=1)
+		logger.debug('configured audio ripper gui window')
 
 def main():
-	logging.basicConfig(filename='./rip_gui.log', filemode='w', level=logging.DEBUG, style="{", format="[{asctime}]{levelname}:{name}:{msg}")
+	logging.basicConfig(filename=path.join(__package__, "./rip_gui.log"), filemode='w', level=logging.DEBUG, style="{", format="[{asctime}]{levelname}:{name}:{msg}")
+	logger = logging.getLogger(__name__)
+	logger.debug('configured logging')
+
 	app = AudioRipperGUI()
 	app.configure_window()
+	logger.debug('configured audio ripper gui')
+
+	logger.debug('starting audio ripper gui main loop...')
 	app.mainloop()
 
 if __name__ == '__main__':
