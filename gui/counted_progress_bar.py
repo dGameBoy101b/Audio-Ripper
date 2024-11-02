8925c9f25a46bc92625488d53617e152ed05c061
dGameBoy101b <rhysmader@gmail.com>
2024-11-02 18:20:37 +0800

    extracted counted progress bar

---
+++ gui/counted_progress_bar.py
@@ -0,0 +1,28 @@
+from tkinter import ttk, DoubleVar, IntVar
+from typing import Callable
+
+class CountedProgressbar(ttk.Frame):
+
+	def default_to_string(value:float|int, max:float|int)->str:
+		return f'{value}/{max}'
+
+	def __init__(self, value:DoubleVar|IntVar, maximum:float|int, to_string:Callable[[float|int, float|int], str]=None, master:ttk.Widget=None):
+		super().__init__(master)
+
+		self.value_variable = value
+		self.maximum = maximum
+		self.to_string = CountedProgressbar.default_to_string if to_string is None else to_string
+		self.progressbar = ttk.Progressbar(self, mode='determinate', maximum=maximum, variable=self.value_variable)
+		self.counter = ttk.Label(self, text=self.to_string(self.value_variable.get(), self.maximum))
+		self.__callback_name = self.value_variable.trace_add('write', self.__update)
+
+		self.progressbar.grid(column=0, row=0, sticky='EW')
+		self.counter.grid(column=1, row=0)
+		self.columnconfigure(0, weight=1)
+
+	def __update(self, var, index, mode):
+		self.counter['text'] = self.to_string(self.value_variable.get(), self.maximum)
+
+	def destroy(self):
+		self.value_variable.trace_remove('write', self.__callback_name)
+		return super().destroy()
--- gui/input_directory_item.py
+++ gui/input_directory_item.py
@@ -5,6 +5,7 @@ from ..scan_for_audio import is_audio, is_directory
 from tkinter import StringVar, IntVar
 from logging import getLogger
 from os.path import abspath
+from .counted_progress_bar import CountedProgressbar
 
 class InputDirectoryItem(ttk.Frame):
 
@@ -23,14 +24,14 @@ class InputDirectoryItem(ttk.Frame):
 		self.text_variable = StringVar(self, value=abspath(path))
 		self.text = ttk.Entry(self, textvariable=self.text_variable, state='readonly', width=30)
 		self.remove_button = ttk.Button(self, text='x', command=self.remove, width=2)
-		self.progress_variable = IntVar(self)
-		self.progress_max = len(listdir(path))
-		self.progress_bar = ttk.Progressbar(self, orient='horizontal', mode='determinate', maximum=self.progress_max, variable=self.progress_variable)
+		progress_max = len(listdir(path))
+		progress_variable = IntVar(self, value=0)
+		self.progress = CountedProgressbar(progress_variable, progress_max, master=self)
 		logger.debug('created input directory item children')
 
 		self.text.grid(column=0, row=0, sticky='EW')
 		self.remove_button.grid(column=1, row=0)
-		self.progress_bar.grid(column=0, columnspan=2, row=1, sticky='EW')
+		self.progress.grid(column=0, columnspan=2, row=1, sticky='EW')
 		self.columnconfigure(0, weight=1)
 		logger.debug('layed out input directory item')
 		
@@ -39,7 +40,7 @@ class InputDirectoryItem(ttk.Frame):
 		logger.info(f'scanning directory: {path}')
 
 	def __schedule_next(self):
-		self.task_id = self.after(100, self.__scan)
+		self.task_id = self.after(0, self.__scan)
 
 	def __scan(self):
 		logger = getLogger(__name__)
@@ -48,7 +49,7 @@ class InputDirectoryItem(ttk.Frame):
 			entry = next(self.scanner)
 		except StopIteration:
 			self.task_id = None
-			self.progress_variable.set(self.progress_max)
+			self.progress.value_variable.set(self.progress.maximum)
 			logger.info(f'scan complete: {self.path}')
 			self.remove()
 			return
@@ -62,12 +63,12 @@ class InputDirectoryItem(ttk.Frame):
 					logger.debug(f'found an audio file: {entry}')
 					self.on_audio(entry)
 			except RuntimeError as x:
-				logger.error(exc_info=x)
+				logger.error(f'failed to scan: {entry}', exc_info=x)
 		else:
 			logger.debug(f'not scanning: {entry}')
 
-		self.progress_variable.set(self.progress_variable.get() + 1)
-		logger.info(f'progressed scan {self.progress_variable.get()} / {self.progress_max}: {self.path}')
+		self.progress.value_variable.set(self.progress.value_variable.get() + 1)
+		logger.info(f'progressed scan {self.progress.value_variable.get()} / {self.progress.maximum}: {self.path}')
 
 		self.__schedule_next()
 
@@ -79,6 +80,9 @@ class InputDirectoryItem(ttk.Frame):
 		logger.debug(f'removed input directory: {self.path}')
 
 	def destroy(self):
-		if self.task_id is not None:
-			self.after_cancel(self.task_id)
+		try:
+			if self.task_id is not None:
+				self.after_cancel(self.task_id)
+		except AttributeError:
+			pass
 		super().destroy()
