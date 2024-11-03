7eace5325a704559961988af4246b9c892f67f8f
dGameBoy101b <rhysmader@gmail.com>
2024-11-03 22:00:33 +0800

    extracted logging config. added filter to logging config to exclude ffprobe logs

--- audio_ripper.py
+++ audio_ripper.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser, Namespace
+import logging.config
 import sys
 from pathlib import PurePath
 import logging
 from .threaded_rip import rip_threaded
 from .rip_args import RipArgs
 from os import path
+from .configure_logging import config_dict
 
 def create_parser()->ArgumentParser:
 	logger = logging.getLogger(__name__)
@@ -49,7 +51,7 @@ def bundle_args(args: Namespace)->RipArgs:
 	return RipArgs(args.output_dir, args.input_dir, args.output_filetype, dict(args.metadata_overrides))
 
 def main():
-	logging.basicConfig(filename=path.join(path.dirname(__file__), 'rip.log'), filemode="w", level=logging.DEBUG, style="{", format="[{asctime}]{levelname}:{name}:{msg}")
+	logging.config.dictConfig(config_dict)
 	logger = logging.getLogger(__name__)
 	parser = create_parser()
 	sys_args = sys.argv[1:]
---
+++ configure_logging.py
@@ -0,0 +1,40 @@
+from logging import DEBUG
+from os.path import join, dirname
+from .exclude_filter import ExcludeFilter
+
+config_dict = {
+	'version': 1,
+	'formatters':{
+		'default' : {
+			'format': "[{asctime}]{levelname}:{name}:{msg}",
+			'style': "{"
+		}
+	},
+	'filters':{
+		'not_ffprobe':{
+			'()': ExcludeFilter,
+			'name': 'ffprobe'
+		}
+	},
+	'handlers':{
+		'file':{
+			'class': 'logging.FileHandler',
+			'formatter': 'default',
+			'level': DEBUG,
+			'filename': join(dirname(__file__), 'rip.log'),
+			'mode' : 'w',
+			'filters':[
+				'not_ffprobe'
+			]
+		}
+	},
+	'loggers':{
+		'root':{
+			'level':DEBUG,
+			'handlers':[
+				'file'
+			]
+		}
+	}
+}
+	
