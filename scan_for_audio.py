033353bb00d62179934b5147f3d57c027508aa35
dGameBoy101b <rhysmader@gmail.com>
2024-11-03 22:06:08 +0800

    removed file, directory, and audio detection global methods

--- scan_for_audio.py
+++ scan_for_audio.py
@@ -2,25 +2,7 @@ import os
 import os.path
 import logging
 from collections.abc import Iterator
-from ffprobe import FFProbe
 from .audio_scanner import AudioScanner
-
-def is_file(path:os.PathLike)->bool:
-	return os.path.isfile(path)
-
-def is_audio(path:os.PathLike)->bool:
-	logger = logging.getLogger(__name__)
-	if not is_file(path):
-		return False
-	logger.debug(f'probing file: {path}')
-	try:
-		probe = FFProbe(path)
-	except Exception as x:
-		raise RuntimeError(f"Failed to probe file: {path}", x)
-	return len(probe.audio) > 0
-	
-def is_directory(path:os.PathLike)->bool:
-	return os.path.isdir(path)
 
 def scan_for_audio(*root_paths:list[os.PathLike])->Iterator[os.PathLike]:
 	with AudioScanner(root_paths) as scanner:
