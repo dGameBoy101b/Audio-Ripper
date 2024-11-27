a4ba3cb17f1eda7645e1f82d76ef75ea53854d06
dGameBoy101b <rhysmader@gmail.com>
2024-11-27 21:19:52 +0800

    Changed rip report to print conversions as file paths.

--- rip_report.py
+++ rip_report.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
+from os import PathLike, fspath
 from .rip_args import RipArgs
 
 @dataclass(frozen=True)
 class RipReport:
 	args: RipArgs
-	conversions:dict
-	seconds_to_rip:float
+	conversions: dict[PathLike, PathLike]
+	seconds_to_rip: float
 
 	def __str__(self)->str:
 		return (f'{self.args}'
-		+f'\n{len(self.conversions)} conversions:\n'+'\n'.join([f'\t{input} -> {output}' for (input, output) in self.conversions.items()])
+		+f'\n{len(self.conversions)} conversions:\n'+'\n'.join([f'\t{fspath(input)} -> {fspath(output)}' for (input, output) in self.conversions.items()])
 		+f'\ntook {self.seconds_to_rip} seconds')
