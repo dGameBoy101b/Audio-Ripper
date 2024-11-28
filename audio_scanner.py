fae0821b1198f2ae3269f29144fb547d14d98129
dGameBoy101b <rhysmader@gmail.com>
2024-11-28 17:21:52 +0800

    Changed audio scanner to no longer skip items in its input queue. Extracted submethods from audio scanner continue scan.

--- audio_scanner.py
+++ audio_scanner.py
@@ -1,6 +1,6 @@
 from logging import getLogger
 from os import PathLike, fspath, scandir
-from queue import Queue
+from queue import Empty, Queue
 from typing import Callable, Generator, Self
 from os.path import isdir, isfile
 from ffprobe import FFProbe
@@ -40,61 +40,108 @@ class AudioScanner():
 			raise RuntimeError(f"Failed to probe file: {fspath(path)}", x)
 		return len(probe.audio) > 0
 
+	def try_output_skip(self, path:PathLike)->bool:
+		if self.should_skip(path):
+			return False
+		self.output_skip(path)
+		return True
+	
+	def output_skip(self, path:PathLike):
+		logger = getLogger(__name__)
+		logger.debug(f'skipped scanned path: {fspath(path)}')
+		self.output_skipped.put(path)
+	
+	def try_output_directory(self, path:PathLike)->bool:
+		if not self.is_directory(path):
+			return False
+		self.output_directory(path)
+		return True
+	
+	def output_directory(self, directory:PathLike):
+		logger = getLogger(__name__)
+		logger.debug(f'found directory: {fspath(directory)}')
+		self.output_subdirectories.put(directory)
+	
+	def try_output_audio(self, path:PathLike)->bool:
+		logger = getLogger(__name__)
+		try:
+			is_audio = self.is_audio(path)
+		except RuntimeError as x:
+			logger.error(f'failed to identify audio: {fspath(path)}', exc_info=x)
+			return False
+		if not is_audio:
+			return False
+		self.output_audio(path)
+		return True
+		
+	def output_audio(self, audio:PathLike):
+		logger = getLogger(__name__)
+		logger.debug(f'found audio: {fspath(audio)}')
+		self.output_audio.put(audio)
+
+	def open_next_directory(self):
+		logger = getLogger(__name__)
+		if self.__scanner is not None:
+			self.close_current_directory()
+		try:
+			directory = self.input_directories.get(False)
+		except Empty:
+			raise StopIteration()
+		logger.info(f'started scanning directory: {fspath(directory)}')
+		self.__current_path = None
+		self.__current_directory = directory
+		self.__scanner = scandir(directory)
+
+	def try_get_next_path(self)->bool:
+		logger = getLogger(__name__)
+		if self.__current_path is not None:
+			return True
+		if self.__scanner is None:
+			return False
+		try:
+			self.__current_path = next(self.__scanner)
+		except StopIteration:
+			logger.info(f'finished scanning directory: {fspath(self.__current_directory)}')
+			self.close_current_directory()
+			return False
+		return True
+
 	def continue_scan(self):
 		logger = getLogger(__name__)
 
 		if self.__scanner is None:
-			if self.input_directories.empty():
-				raise StopIteration()
-			directory = self.input_directories.get()
-			if self.should_skip(directory):
-				logger.debug(f'skipped scanning directory: {fspath(directory)}')
-				self.__current_path = None
-				self.output_skipped.put(directory)
-				return
-			logger.info(f'started scanning directory: {fspath(directory)}')
-			self.__current_path = None
-			self.__current_directory = directory
-			self.__scanner = scandir(directory)
+			self.open_next_directory()
 			return
 
-		if self.__current_path is None:
-			try:
-				self.__current_path = next(self.__scanner)
-			except StopIteration:
-				logger.info(f'finished scanning directory: {fspath(self.__current_directory)}')
-				self.close_current_directory()
-				return
+		if not self.try_get_next_path():
+			return
 		
-		if not self.should_skip(self.__current_path):
-			if self.is_directory(self.__current_path):
-				logger.debug(f'found directory: {fspath(self.__current_path)}')
-				self.output_subdirectories.put(self.__current_path)
-				self.__current_path = None
-				return
-			
-			try:
-				if self.is_audio(self.__current_path):
-					logger.debug(f'found audio: {fspath(self.__current_path)}')
-					self.output_audio.put(self.__current_path)
-					self.__current_path = None
-					return
-			except RuntimeError as x:
-				logger.error(f'failed to identify audio: {fspath(self.__current_path)}', exc_info=x)
+		if self.try_output_skip(self.__current_path):
+			self.__current_path = None
+			return
 		
-		logger.debug(f'skipped scanned path: {fspath(self.__current_path)}')
-		self.on_skip(self.__current_directory, self.__current_path)
+		if self.try_output_directory(self.__current_path):
+			self.__current_path = None
+			return
+		
+		if self.try_output_audio(self.__current_path):
+			self.__current_path = None
+			return
+		
+		self.output_skip(self.__current_path)
+		self.__current_path = None
 
 	def close_current_directory(self):
 		logger = getLogger(__name__)
-		if self.__scanner is not None:
-			self.__scanner.close()
-			self.__scanner = None
-			logger.info(f'closed directory: {fspath(self.__current_directory)}')
-			self.__current_directory = None
-			self.__current_path = None
-			self.input_directories.task_done()
-			logger.debug(f'{self.input_directories.unfinished_tasks} unfinished directories')
+		if self.__scanner is None:
+			return
+		self.__scanner.close()
+		self.__scanner = None
+		logger.info(f'closed directory: {fspath(self.__current_directory)}')
+		self.__current_directory = None
+		self.__current_path = None
+		self.input_directories.task_done()
+		logger.debug(f'{self.input_directories.unfinished_tasks} unfinished directories')
 
 	def __del__(self):
 		self.close_current_directory()
