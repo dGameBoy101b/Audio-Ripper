85495b41fbdcebec7adc0fe4053ade24961d8b23
dGameBoy101b <rhysmader@gmail.com>
2024-12-21 23:48:34 +0800

    Extracted output directory frame

---
+++ gui/output_directory_frame.py
@@ -0,0 +1,56 @@
+from logging import getLogger
+from os import PathLike, fspath
+from os.path import isdir
+from posixpath import abspath
+from tkinter import StringVar
+from tkinter.ttk import Button, Entry, Frame, Label
+from tkinter.filedialog import askdirectory
+
+
+class OutputDirectoryFrame(Frame):
+	
+	def __init__(self, master=None, **kwargs):
+		super().__init__(master, **kwargs)
+		self.__create_widgets()
+		self.__config_grid()
+
+	def __create_widgets(self):
+		logger = getLogger(__name__)
+		logger.debug(f'creating widgets... {self}')
+		self.variable = StringVar(self)
+		self.label = Label(self, text='Output Directory')
+		self.entry = Entry(self, justify='left', textvariable=self.variable)
+		self.browse_button = Button(self, text='Browse...', command=self.ask)
+		logger.debug(f'widgets created: {self}')
+
+	def __config_grid(self):
+		logger = getLogger(__name__)
+		logger.debug(f'configuring grid... {self}')
+		self.label.grid(row=0, column=0)
+		self.entry.grid(row=0, column=1, sticky='EW')
+		self.browse_button.grid(row=0, column=2)
+		self.columnconfigure(1, weight=1)
+		logger.debug(f'grid configured: {self}')
+
+	def ask(self)->bool:
+		logger = getLogger(__name__)
+		directory = askdirectory(title="Output Directory")
+		if directory == '':
+			logger.info('no output directory selected')
+			return False
+		try:
+			self.set(directory)
+		except BaseException as x:
+			logger.warning(f'user selected invalid out directory: {directory}', exc_info=x)
+			return False
+		return True
+
+	def set(self, directory:PathLike):
+		logger = getLogger(__name__)
+		if not isdir(directory):
+			raise ValueError(f'non-directory given as output directory: {fspath(directory)}')
+		self.variable.set(abspath(directory))
+		logger.info(f'set output directory: {fspath(directory)}')
+	
+	def get(self)->PathLike:
+		return self.variable.get()
--- gui/output_frame.py
+++ gui/output_frame.py
@@ -1,10 +1,7 @@
 from logging import getLogger
-from os import PathLike, fspath
-from os.path import isdir, abspath
-from tkinter.ttk import Labelframe, Label, Entry, Button
-from tkinter import StringVar
-from tkinter.filedialog import askdirectory
+from tkinter.ttk import Labelframe, Button
 
+from .output_directory_frame import OutputDirectoryFrame
 from .conversions_frame import ConversionsFrame
 
 class OutputFrame(Labelframe):
@@ -17,10 +14,7 @@ class OutputFrame(Labelframe):
 	def __create_widgets(self):
 		logger = getLogger(__name__)
 		logger.debug(f'creating widgets... {self}')
-		self.directory_variable = StringVar(self)
-		self.directory_label = Label(self, text='Output Directory')
-		self.directory_entry = Entry(self, justify='left', textvariable=self.directory_variable)
-		self.directory_button = Button(self, text='Browse...', command=self.set_directory)
+		self.directory = OutputDirectoryFrame(self)
 		self.rip_button = Button(self, text='Start Rip', command=self.start_rip)
 		self.conversions_frame = ConversionsFrame(self)
 		logger.debug(f'widgets created: {self}')
@@ -28,31 +22,12 @@ class OutputFrame(Labelframe):
 	def __config_grid(self):
 		logger = getLogger(__name__)
 		logger.debug(f'configuring grid... {self}')
-		self.directory_label.grid(row=0, column=0)
-		self.directory_entry.grid(row=0, column=1, sticky='EW')
-		self.directory_button.grid(row=0, column=2)
-		self.rip_button.grid(row=1, column=0, columnspan=3, sticky='EW')
-		self.conversions_frame.grid(row=2, column=0, columnspan=3, sticky='NSEW')
-		self.columnconfigure(1, weight=1)
+		self.directory.grid(row=0, column=0, sticky='EW')
+		self.rip_button.grid(row=1, column=0, sticky='EW')
+		self.conversions_frame.grid(row=2, column=0, sticky='NSEW')
+		self.columnconfigure(0, weight=1)
 		self.rowconfigure(2, weight=1)
 		logger.debug(f'grid configured: {self}')
-
-	def set_directory(self, directory:PathLike=None)->bool:
-		logger = getLogger(__name__)
-
-		if directory is None:
-			directory = askdirectory(title="Output Directory")
-			if directory == '':
-				logger.info('no output directory selected')
-				return False
-		
-		if not isdir(directory):
-			logger.warning(f'non-directory given as output directory: {fspath(directory)}')
-			return False
-		
-		self.directory_variable.set(abspath(directory))
-		logger.info(f'set output directory: {fspath(directory)}')
-		return True
 
 	def start_rip(self):
 		logger = getLogger(__name__)
