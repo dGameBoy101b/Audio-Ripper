47888c155fd8d2e2c0677095d750793a4f0809f5
dGameBoy101b <rhysmader@gmail.com>
2024-12-26 18:05:09 +0800

    Refacted metadata overrides frame. Added logging. Changed remove method to take a key parameter instead. Fixed laying out destroyed widgets.

--- gui/metadata_overrides_frame.py
+++ gui/metadata_overrides_frame.py
@@ -10,15 +10,22 @@ class MetadataOverridesFrame(LabelFrame):
 
 	def __init__(self, master = None):
 		super().__init__(master, text='Metadata Overrides')
-
-		self.__items:list[MetadataOverrideItem] = list()
 		self.__destroy_bindings = dict()
+		self.__create_widgets()
+		self.__configure_grid()
 
+	def __create_widgets(self):
+		logger = getLogger(__name__)
+		logger.debug(f'creating widgets... {self}')
 		self.content_box = VerticalBox(self)
 		self.key_label = Label(self.content_box, text='Key', anchor='center')
 		self.value_label = Label(self.content_box, text='Value', anchor='center')
 		self.add_button = Button(self.content_box, text='+', command=self.add, width=2)
+		logger.debug(f'widgets created: {self}')
 
+	def __configure_grid(self):
+		logger = getLogger(__name__)
+		logger.debug(f'configuring grid... {self}')
 		self.key_label.grid(row=0, column=0, sticky='EW')
 		self.value_label.grid(row=0, column=1, sticky='EW')
 		self.add_button.grid(row=0, column=2)
@@ -35,46 +42,55 @@ class MetadataOverridesFrame(LabelFrame):
 
 		self.columnconfigure(0, weight=1)
 		self.rowconfigure(0, weight=1)
+		logger.debug(f'grid configured: {self}')
 
 	def __layout_items(self):
 		logger = getLogger(__name__)
-		for row in range(len(self.__items)):
-			item = self.__items[row]
+		row = 0
+		for item in self.content_box.content.winfo_children():
+			if not item.winfo_exists():
+				continue
 			item.grid(column=0, row=row, sticky='EW', columnspan=3)
+			row += 1
 		logger.info(f'layed out {len(self.__items)} metadata overrides')
 		self.update()
 
 	def __item_destroyed(self, event: Event):
-		self.remove(event.widget)
+		item:MetadataOverrideItem = event.widget
+		print(f'removed metadata override: {item}')
+		item.unbind('<Destroy>', self.__destroy_bindings[item])
+		del self.__destroy_bindings[item]
+		self.__layout_items()
 
 	def add(self, key:str='', value:str=''):
 		item = MetadataOverrideItem(self.content_box.content, key, value)
-		self.__items.append(item)
 		binding = item.bind('<Destroy>', self.__item_destroyed)
 		self.__destroy_bindings[item] = binding
 		for widget in explore_descendants(item):
 			self.content_box.bind_scroll_forwarding(widget)
 		self.__layout_items()
 
-	def remove(self, item:MetadataOverrideItem):
-		print(f'removed {item} at index {self.__items.index(item)}')
-		self.__items.remove(item)
-		item.unbind('<Destroy>', self.__destroy_bindings[item])
-		del self.__destroy_bindings[item]
-		self.__layout_items()
+	def remove(self, key:str):
+		logger = getLogger(__name__)
+		missing = True
+		for item in self.content_box.content.winfo_children():
+			if item.winfo_exists() and isinstance(item, MetadataOverrideItem) and item.get_key() == key:
+				logger.info(f'removed metadata override: {item}')
+				item.destroy()
+				missing = False
+		if missing:
+			raise KeyError(key)
 
 	def get(self)->dict[str, str]:
 		result = dict()
-		for item in self.__items():
-			if isinstance(item, MetadataOverrideItem):
-				pair = item.get()
-				result[pair[0]] = pair[1]
+		for item in self.content_box.content.winfo_children():
+			if item.winfo_exists() and isinstance(item, MetadataOverrideItem):
+				result[item.get_key()] = item.get_value()
 		return result
 	
 	def destroy(self):
-		for item in self.__items:
+		for item in self.content_box.content.winfo_children():
 			item.unbind('<Destroy>', self.__destroy_bindings[item])
 		self.__destroy_bindings.clear()
-		self.__items.clear()
 		return super().destroy()
 			
