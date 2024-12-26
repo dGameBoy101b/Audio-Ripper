8165b65606888b7698689a2e40bf41e1504a149a
dGameBoy101b <rhysmader@gmail.com>
2024-12-26 19:41:24 +0800

    Added attribute getters to audio copy job

--- audio_copy_job.py
+++ audio_copy_job.py
@@ -1,7 +1,17 @@
 from os import PathLike
+from typing import Any
 from .copy_media import copy_media
 from .job_executor import Job
 
 class AudioCopyJob(Job):
-	def __init__(self, output_path:PathLike, input_path:PathLike,output_args:dict[str,any]):
+	def __init__(self, output_path:PathLike, input_path:PathLike,output_args:dict[str,Any]):
 		super().__init__(copy_media, (output_path, input_path), output_args)
+
+	def __getattr__(self, name:str)->Any:
+		if name == 'output_path':
+			return self.args[0]
+		if name == 'input_path':
+			return self.args[1]
+		if name == 'output_args':
+			return self.kwargs
+		raise AttributeError(name=name, object=self)
