f3c6d6e4d58f895a3fa9ae16c10f059545f3313a
dGameBoy101b <rhysmader@gmail.com>
2024-12-28 01:16:54 +0800

    Added estroy button to output item

--- gui/output_file_item.py
+++ gui/output_file_item.py
@@ -2,7 +2,7 @@ from concurrent.futures import Future
 from logging import getLogger
 from os import PathLike
 from tkinter import DoubleVar, Misc, StringVar
-from tkinter.ttk import Entry, Frame
+from tkinter.ttk import Button, Entry, Frame
 
 from .recurring_tkinter_task import ReccuringTkinterTask
 from .realtime_progressbar import RealtimeProgressbar
@@ -22,13 +22,15 @@ class OutputFileItem(Frame):
 		self.path_entry = Entry(self, textvariable=self.path_variable, state='readonly')
 		self.progress_variable = DoubleVar(self)
 		self.progress_bar = RealtimeProgressbar(rate=200, master=self, mode='indeterminate', variable=self.progress_variable)
+		self.destory_button = Button(master=self, command=self.destroy, text='x', width=2)
 		logger.debug(f'widgets created: {self}')
 
 	def __configure_grid(self):
 		logger = getLogger(__name__)
 		logger.debug(f'configuring grid... {self}')
 		self.path_entry.grid(row=0, column=0, sticky='EW')
-		self.progress_bar.grid(row=1, column=0, sticky='EW')
+		self.destory_button.grid(row=0, column=1)
+		self.progress_bar.grid(row=1, column=0, columnspan=2, sticky='EW')
 		self.columnconfigure(0, weight=1)
 		logger.debug(f'grid configured: {self}')
 
@@ -62,3 +64,11 @@ class OutputFileItem(Frame):
 			self.progress_variable.set(100)
 			return
 		logger.debug(f'no progress found: {self.path_variable.get()}')
+
+	def destory(self):
+		logger = getLogger(__name__)
+		self.check_progress_task.unschedule()
+		self.progress_bar.stop()
+		if self.future is not None and not self.future.done():
+			if not self.future.cancel():
+				logger.warning(f'failed to cancel running rip job: {self.path_variable.get()}')
