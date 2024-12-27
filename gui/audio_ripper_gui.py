7cf05395accded58804fe846b88551cfbe872c49
dGameBoy101b <rhysmader@gmail.com>
2024-12-27 23:21:22 +0800

    Halved max worker processes.

--- gui/audio_ripper_gui.py
+++ gui/audio_ripper_gui.py
@@ -2,6 +2,7 @@ from logging import getLogger
 from logging.config import dictConfig
 from tkinter.ttk import PanedWindow
 from concurrent.futures import ProcessPoolExecutor
+from os import cpu_count
 
 from ..job_executor import JobExecutor
 from ..audio_scanner import AudioScanner, is_audio
@@ -13,6 +14,15 @@ from .output_files_frame import OutputFilesFrame
 from .configure_logging import config_dict
 
 class AudioRipperGUI(PanedWindow):
+	MAX_WORKERS = 60
+
+	def max_workers()->int:
+		max_workers = cpu_count()
+		if max_workers is None:
+			max_workers = 1
+		elif max_workers > 1:
+			max_workers //= 2
+		return min(max_workers, AudioRipperGUI.MAX_WORKERS)
 
 	def __init__(self, master=None):
 		super().__init__(master, orient='horizontal')
@@ -26,7 +36,7 @@ class AudioRipperGUI(PanedWindow):
 		scanner = AudioScanner()
 		self.files_frame = InputFilesFrame(is_audio, self)
 		self.scans_frame = DirectoryScansFrame(self.files_frame, scanner, self)
-		executor = ProcessPoolExecutor()
+		executor = ProcessPoolExecutor(AudioRipperGUI.max_workers())
 		job_executor = JobExecutor(executor)
 		self.output_frame = OutputFilesFrame(job_executor, self.files_frame, self.settings_frame, self)
 		logger.debug(f'created widgets: {self}')
