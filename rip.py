15d81b02a654b6ac44601eb33fa631d32c6c2094
dGameBoy101b <rhysmader@gmail.com>
2025-01-01 15:54:43 +0800

    Removed dependency on job executor.

--- rip.py
+++ rip.py
@@ -1,9 +1,9 @@
-from concurrent.futures import Executor, wait
+from concurrent.futures import Executor, Future, wait
 import logging
 import time
-from .audio_copy_job import AudioCopyJob
+
+from .copy_media import copy_media
 from .audio_scanner import AudioScanner
-from .job_executor import JobExecutor
 from .rip_args import RipArgs
 from .rip_report import RipReport
 
@@ -13,22 +13,19 @@ def rip(args:RipArgs, executor:Executor)->RipReport:
 
 	logger.info(f'beginning rip from {args.input_dir} to {args.output_dir} with {args.output_extension} output type and following metadata overrides: {"\n".join(args.metadata_overrides)}')
 	start_time = time.perf_counter()
-	with JobExecutor(executor) as job_executor:
+	futures:list[Future] = list()
 
-		logger.debug('creating jobs...')
-		with AudioScanner([args.input_dir]) as scanner:
-			for audio_path in scanner:
-				output_path = args.output_path(audio_path)
-				conversions[audio_path] = output_path
-				job = AudioCopyJob(output_path, audio_path, args.output_args)
-				job_executor.jobs.put(job)
-		
-		logger.debug(f'starting {job_executor.jobs.qsize()} jobs...')
-		futures = job_executor.start_jobs()
+	logger.debug('submitting jobs...')
+	with AudioScanner([args.input_dir]) as scanner:
+		for audio_path in scanner:
+			output_path = args.output_path(audio_path)
+			conversions[audio_path] = output_path
+			future = executor.submit(copy_media, output_path, audio_path, **args.output_args)
+			futures.append(future)
 
-		logger.debug(f'waiting for {len(futures)} jobs...')
-		wait(futures)
+	logger.debug(f'waiting for {len(futures)} futures...')
+	wait(futures)
 
-	duration = time.perf_counter()-start_time
+	duration = time.perf_counter() - start_time
 	logger.info(f'ripped {len(conversions)} audio files in {duration} seconds')
 	return RipReport(args, conversions, duration)
