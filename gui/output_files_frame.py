5449c2684cb5d92c83bb08a4111cd78584209a79
dGameBoy101b <rhysmader@gmail.com>
2025-01-01 14:51:42 +0800

    Added preview output button.

--- gui/output_files_frame.py
+++ gui/output_files_frame.py
@@ -28,6 +28,7 @@ class OutputFilesFrame(LabelFrame):
 	def __create_widgets(self):
 		logger = getLogger(__name__)
 		logger.debug(f'creating widgets... {self}')
+		self.preview_button = Button(self, text='Preview', command=self.show_preview)
 		self.rip_button = Button(self, text='Start Rip', command=self.start_rip)
 		self.clear_button = Button(self, text='Clear Ouput', command=self.clear)
 		self.content_box = VerticalBox(self)
@@ -36,11 +37,12 @@ class OutputFilesFrame(LabelFrame):
 	def __config_grid(self):
 		logger = getLogger(__name__)
 		logger.debug(f'configuring grid... {self}')
-		self.rip_button.grid(row=0, column=0, sticky='EW')
-		self.clear_button.grid(row=0, column=1, sticky='EW')
-		self.content_box.grid(row=1, column=0, columnspan=2, sticky='NSEW')
+		self.preview_button.grid(row=0, column=0, sticky='EW')
+		self.rip_button.grid(row=0, column=1, sticky='EW')
+		self.clear_button.grid(row=0, column=2, sticky='EW')
+		self.content_box.grid(row=1, column=0, columnspan=3, sticky='NSEW')
 		self.content_box.content.columnconfigure(0, weight=1)
-		self.columnconfigure([0,1], weight=1)
+		self.columnconfigure([0,1,2], weight=1)
 		self.rowconfigure(1, weight=1)
 		logger.debug(f'grid configured: {self}')
 
@@ -53,48 +55,64 @@ class OutputFilesFrame(LabelFrame):
 				row += 1
 		logger.debug(f'layed out {row} output file items')
 
-	def output_paths(self)->Iterable[Tuple[PathLike, PathLike]]:
+	def input_paths(self)->Tuple[PathLike]:
+		return tuple(self.input_files.get())
+
+	def output_paths(self, input_paths:Iterable[PathLike])->Iterable[PathLike]:
 		logger = getLogger(__name__)
 		logger.debug(f'generating output paths... {self}')
-		files = tuple(self.input_files.get())
 		output_dir = self.settings.directory.get()
 		logger.debug(f'output directory: {abspath(output_dir)}')
 		output_extension = self.settings.file_extension.get()
 		logger.debug(f'output extension: {output_extension}')
-		for input_path in files:
+		for input_path in input_paths:
 			output_path = abspath(join(output_dir, change_file_extension(basename(input_path), output_extension)))
 			logger.debug(f'generated output path: {input_path} -> {output_path}')
-			yield (input_path, output_path)
+			yield output_path
 
-	def __create_futures(self)->Iterable[Tuple[PathLike, Future]]:
+	def __submit_jobs(self, input_paths:Iterable[PathLike], output_paths:Iterable[PathLike])->Iterable[Future]:
 		logger = getLogger(__name__)
-		logger.debug(f'creating jobs... {self}')
+		logger.debug(f'submitting jobs... {self}')
 		metadata_overrides = self.settings.metadata_overrides.get()
 		logger.debug(f'metadata overrides: {''.join([f'{key}={metadata_overrides[key]}\n' for key in metadata_overrides])}')
 		output_args = override_media_metadata(**metadata_overrides)
-		for input_path, output_path in self.output_paths():
+		for input_path, output_path in zip(input_paths, output_paths, strict=True):
 			future = self.executor.submit(copy_media, output_path, input_path, **output_args)
-			logger.debug(f'created job {input_path}->{output_path}: {self}')
-			yield (output_path, future)
+			logger.debug(f'submitted job {input_path}->{output_path}: {self}')
+			yield future
 
-	def __create_items(self)->list[OutputFileItem]:
+	def __create_items(self, output_paths:Iterable[PathLike])->list[OutputFileItem]:
 		logger = getLogger(__name__)
 		logger.debug(f'creating items... {self}')
 		items = list()
-		for path, future in self.__create_futures():
-			item = OutputFileItem(path, future=future, master=self.content_box.content)
+		for path in output_paths:
+			item = OutputFileItem(path, master=self.content_box.content)
 			items.append(item)
+			logger.debug(f'created item: {path} {item}')
 			for child in explore_descendants(item):
 				self.content_box.bind_scroll_forwarding(child)
 			self.__layout_items()
 			self.update()
 		logger.debug(f'{len(items)} items created')
 		return items
+	
+	def show_preview(self):
+		logger = getLogger(__name__)
+		logger.info('generating preview...')
+		self.clear()
+		self.__create_items(self.output_paths(self.input_paths()))
+		logger.info('preview generated')
 		
 	def start_rip(self):
 		logger = getLogger(__name__)
 		logger.info('starting rip...')
-		items = self.__create_items()
+		self.clear()
+		input_paths = self.input_paths()
+		output_paths = tuple(self.output_paths(input_paths))
+		items = self.__create_items(output_paths)
+		futures = self.__submit_jobs(input_paths, output_paths)
+		for item, future in zip(items, futures, strict=True):
+			item.started(future)
 		logger.info(f'started {len(items)} rip jobs')
 
 	def clear(self):
