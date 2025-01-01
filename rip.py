1dcea22f3b633942ede40e80fff8f6aa5138918d
dGameBoy101b <rhysmader@gmail.com>
2025-01-01 17:08:37 +0800

    Changed rip to use scan for audio

--- rip.py
+++ rip.py
@@ -1,12 +1,11 @@
 from concurrent.futures import Executor, Future, wait
-from os import scandir
 import logging
 import time
 
-from .is_audio import is_audio
 from .copy_media import copy_media
 from .rip_args import RipArgs
 from .rip_report import RipReport
+from .scan_for_audio import scan_for_audio
 
 def rip(args:RipArgs, executor:Executor)->RipReport:
 	logger = logging.getLogger(__name__)
@@ -17,9 +16,7 @@ def rip(args:RipArgs, executor:Executor)->RipReport:
 	futures:list[Future] = list()
 
 	logger.debug('submitting jobs...')
-	for input_path in scandir(args.input_dir):
-		if not is_audio(input_path):
-			continue
+	for input_path in scan_for_audio(args.input_dir):
 		output_path = args.output_path(input_path)
 		conversions[input_path] = output_path
 		future = executor.submit(copy_media, output_path, input_path, **args.output_args)
